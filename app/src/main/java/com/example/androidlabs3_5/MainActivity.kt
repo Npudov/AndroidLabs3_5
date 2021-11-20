6f801f069d7e5dd800f1c06811aec972a29e69a4
somagic2 <nik.p008@gmail.com>
2021-11-20 19:08:50 +0300

    some fix

--- app/src/main/java/com/example/androidlabs3_5/MainActivity.kt
+++ app/src/main/java/com/example/androidlabs3_5/MainActivity.kt
@@ -12,14 +12,14 @@ import com.example.androidlabs3_5.databinding.ActivityMainBinding
 open class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainBinding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
 
         val navController = findNavController(R.id.nav_host_fragment)
