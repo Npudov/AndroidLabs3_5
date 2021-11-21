2c50ce790365c4269559183617aa4ab241e05860
somagic2 <nik.p008@gmail.com>
2021-11-22 00:15:09 +0300

    cosmetic fix

--- app/src/androidTest/java/com/example/androidlabs3_5/AboutUtils.kt
+++ app/src/androidTest/java/com/example/androidlabs3_5/AboutUtils.kt
@@ -1,14 +1,8 @@
 package com.example.androidlabs3_5
 
-import android.view.Gravity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openContextualActionModeOverflowMenu
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.DrawerActions
-import androidx.test.espresso.contrib.DrawerMatchers.isClosed
-import androidx.test.espresso.contrib.NavigationViewActions
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 
 
